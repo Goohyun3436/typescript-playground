cd2e4cdd6f6606a3a62c3995502e5c53d2967c3d
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2023-02-01 06:26:26 +0900

    Add: Main 페이지 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./GlobalStyle";
+import Main from "./pages/Main";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Router>
     </>
   );
 };
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Main = () => {
+  return (
+    <MainBlock>
+      <h1>javascript</h1>
+    </MainBlock>
+  );
+};
+
+const MainBlock = styled.div``;
+
+export default Main;
