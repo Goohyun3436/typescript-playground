db80c4ed8b471847c561fa0d7a32f981f23cd28b
Goo Hyun <rngus3436@g.kmou.ac.kr>
2023-02-19 21:31:31 +0900

    ts 런타임 에러 예시

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Main = () => {
+  // [1, 2, 3, 4] + false  >>  '+' 연산자를 'number[]' 및 'boolean' 형식에 적용할 수 없습니다.
+
+  // function divide(a, b) {  // 'a' 매개 변수에는 암시적으로 'any' 형식이 포함됩니다.
+  //   return a / b;
+  // }
+
+  // divide("hello");  // 2개의 인수가 필요한데 1개를 가져왔습니다.
+
+  return <MainBlock></MainBlock>;
+};
+
+const MainBlock = styled.div``;
+
+export default Main;
