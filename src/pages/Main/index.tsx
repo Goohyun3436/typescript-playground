b669be515ce625717a4ea5c8030a052088358de6
Goo Hyun <rngus3436@g.kmou.ac.kr>
2023-02-21 15:44:50 +0900

    객체 생성 함수와 리턴 타입 지정

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,39 +1,21 @@
 import styled from "styled-components";
 
 const Main = () => {
-  // let a = "str";
-  // a = 1;  // 'number' 형식은 'string' 형식에 할당할 수 없습니다.
-
-  // let b: boolean = "x";  // 'string' 형식은 'boolean' 형식에 할당할 수 없습니다.
-
-  // 배열
-  // let a: number[] = [1, 2, 3];
-  // let b: string[] = ["il", "1"];
-  // let c: boolean[] = [true, false];
-
   // optioner
-  // const player: {
-  //   name: string;
-  //   age?: number;
-  // } = {
-  //   name: "nico",
-  //   age: 1,
-  // };
-  // if(player.age < 10) {}  // 'player.age'은(는) 'undefined'일 수 있습니다.
-
-  // many optioner
-  // type Name = string;å
+  // type Name = string;
   // type Age = number;
   // type Player = { name: Name; age?: Age };
 
-  // const playerNico: Player = {
-  //   name: "nico",
-  // };
+  // function playerMaker(name: string): Player {
+  //   return {
+  //     name,
+  //   };
+  // }
 
-  // const playerLynn: Player = {
-  //   name: "nico",
-  //   age: 1,
-  // };
+  // const nico = playerMaker("nico");
+  // nico.age = 3;
+
+  // console.log(nico); // >> {name: 'nico', age: 3}
 
   return <MainBlock></MainBlock>;
 };
