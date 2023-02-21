87de0b0b7a376846a44bbec7af6153bd74041ce4
Goo Hyun <rngus3436@g.kmou.ac.kr>
2023-02-21 15:47:36 +0900

    객체 생성 함수 화살표 함수로 선언하기

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -2,20 +2,11 @@ import styled from "styled-components";
 
 const Main = () => {
   // optioner
-  // type Name = string;
-  // type Age = number;
-  // type Player = { name: Name; age?: Age };
+  type Name = string;
+  type Age = number;
+  type Player = { name: Name; age?: Age };
 
-  // function playerMaker(name: string): Player {
-  //   return {
-  //     name,
-  //   };
-  // }
-
-  // const nico = playerMaker("nico");
-  // nico.age = 3;
-
-  // console.log(nico); // >> {name: 'nico', age: 3}
+  const playerMaker = (name: string): Player => ({ name });
 
   return <MainBlock></MainBlock>;
 };
