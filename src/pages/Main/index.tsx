33f9169dfc867821abdef624ae6ccd7029584cf9
Goo Hyun <rngus3436@g.kmou.ac.kr>
2023-02-21 16:15:18 +0900

    ts 의 보호장치를 비활성화하는 any type

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,22 +1,18 @@
 import styled from "styled-components";
 
 const Main = () => {
-  // optioner
-  // type Name = string;
-  // type Age = number;
-  // type Player = { readonly name: Name; age?: Age };
+  ["nico", 12, false];
 
-  // const playerMaker = (name: string): Player => ({ name });
-  // const nico = playerMaker("nico");
-  // nico.name = "hello"; //  읽기 전용 속성이므로 'name'에 할당할 수 없습니다.
+  // type 으로 표현
+  const player: [string, number, boolean] = ["nico", 1, true];
 
-  // array
-  // const numbers: readonly number[] = [1, 2, 3];
-  // numbers.push(1); // 'readonly number[]' 형식에 'push' 속성이 없습니다.
+  player[0] = 1; // 'number' 형식은 'string' 형식에 할당할 수 없습니다.
 
-  // string
-  // const names: readonly string[] = ["1", "2"];
-  // names.push(); // 'readonly string[]' 형식에 'push' 속성이 없습니다.
+  // any type
+  const a: any[] = [1, 2, 3];
+  const b: any = true;
+
+  console.log(a + b); // 1,2,3true  (type >> objecttrue)
 
   return <MainBlock></MainBlock>;
 };
