a4b3c6c44ed63a61ad9957bcc12d53ce6b24a450
Goo Hyun <rngus3436@g.kmou.ac.kr>
2023-02-23 19:42:08 +0900

    함수, call signature

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,42 +1,17 @@
 import styled from "styled-components";
 
 const Main = () => {
-  // unknown
-  let a: unknown; // ts 의 보호를 받음 (type 채크를 먼저)
-
-  if (typeof a === "number") {
-    let b = a + 1;
+  //function
+  function add(a: number, b: number): number {
+    return a + b;
   }
 
-  let b = a.toUpperCase(); // 'a'은(는) 'unknown' 형식입니다.
-  // type 체크 해주기
-  if (typeof a === "string") {
-    let b = a.toUpperCase();
-  }
+  // alow func
+  const alowAdd = (a: number, b: number): number => a + b;
 
-  // void
-  function hello() {
-    // (local function) hello(): void  >> 아무것도 리턴하지 않는 함수라는 것을 ts가 인식함
-    console.log("아무것도 return 하지 않음");
-  }
-
-  // never
-  function hello2(): never {
-    // 함수가 절대 return 하지 않을 때 사용
-    throw new Error("return 하지 않고 오류를 발생시키는 함수");
-  }
-
-  function hello3(name: string | number) {
-    // type 이 두가지 이상일 때 사용
-    if (typeof name === "string") {
-      name; // >> (parameter) name: string
-    } else if (typeof name === "number") {
-      name; // >> (parameter) name: number
-    } else {
-      name; // >> (parameter) name: never
-      // 이 스코프는 작동하지 않아야 함.
-    }
-  }
+  // call signature of func
+  type Add = (a: number, b: number) => number;
+  const addCS: Add = (a, b) => a + b;
 
   return <MainBlock></MainBlock>;
 };
