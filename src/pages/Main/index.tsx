6866bb8b79a3ac79c438c2fb58a605c5d03d9e49
Goo Hyun <rngus3436@g.kmou.ac.kr>
2023-02-23 20:01:21 +0900

    over loading example2

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,22 @@
 import styled from "styled-components";
 
 const Main = () => {
-  // over loading example
-  type Add = { (a: number, b: number): number; (a: number, b: string): number };
+  // over loading example2 : 라이브러리 만들때 많이 사용
+  type Config = {
+    path: string;
+    state: object;
+  };
 
-  const add: Add = (a, b) => {
-    if (typeof b === "string") return a;
-    return a + b;
+  type Push = {
+    (path: string): void;
+    (config: Config): void;
+  };
+
+  const push: Push = (config) => {
+    if (typeof config === "string") console.log(config);
+    else {
+      console.log(config.path);
+    }
   };
 
   return <MainBlock></MainBlock>;
