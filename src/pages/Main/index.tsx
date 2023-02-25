db66d03905600edc992d2ea40e19d49de6f2e3a2
Goo Hyun <rngus3436@g.kmou.ac.kr>
2023-02-25 17:48:36 +0900

    classes

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,44 +1,50 @@
 import styled from "styled-components";
 
 const Main = () => {
-  // Conclusions
-  type SuperPrint = <T>(a: T[]) => T;
-  const superPrint: SuperPrint = (arr) => arr[0];
+  // Classes
 
-  // 함수에 바로 geceric type 명시해주기
-  function superPrint2<T>(a: T[]) {
-    return a[0];
+  // in js
+  // class Player {
+  //   constructor(firstName, lastName) {
+  //     this.firstName = firstName;
+  //     this.lastName = lastName;
+  //   }
+  // }
+
+  // in ts
+  abstract class User {
+    constructor(
+      private firstName: string,
+      private lastName: string,
+      public nickName: string,
+      protected proName: string // 자식 클래스에서 사용하고 싶을 때
+    ) {}
+
+    abstract getNickName(): void; // 추상 함수
+
+    getFullName() {
+      return `${this.firstName} ${this.lastName}`;
+    }
   }
 
-  // 직접 type 명시
-  const a = superPrint2<boolean>([1, 2]); // 'number' 형식은 'boolean' 형식에 할당할 수 없습니다.
-  const d = superPrint2<boolean>([true, false]);
+  // class 속성 상속
+  class Player extends User {
+    getNickName() {
+      console.log(this.nickName);
+    }
+  }
 
-  // type 재사용
-  type Player<E> = {
-    name: string;
-    extraInfo: E;
-  };
+  const nico = new Player("nico", "las", "니꼬", "p");
 
-  // example1
-  type NicoExtra = {
-    favFood: string;
-  };
+  // error: private
+  // 'firstName' 속성은 private이며 'Player' 클래스 내에서만 액세스할 수 있습니다.
+  // (js 에서는 가능함)
+  // private 는 변수, 메소드에 모두 적용 가능
+  nico.firstName;
 
-  type NicoPlayer = Player<NicoExtra>;
-
-  const nico: NicoPlayer = {
-    name: "nico",
-    extraInfo: {
-      favFood: "kimchi",
-    },
-  };
-
-  // example2
-  const lynn: Player<null> = {
-    name: "lynn",
-    extraInfo: null,
-  };
+  // error: 추상 클래스로 인스턴트 만들기
+  // 상속만 가능하고 새로운 인스턴트 만들기는 불가능
+  const nico1 = new User("nico", "las", "니꼬");
 
   return <MainBlock></MainBlock>;
 };
